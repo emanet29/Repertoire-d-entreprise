617ddb50de6a2485a3416bee4f261b0914cd06dc
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-06-06 20:26:37 +0200

    AccueilController + PersonneCell

---
+++ Repertoire d'entreprise/Controller/AccueilController.swift
@@ -0,0 +1,17 @@
+//
+//  AccueilController.swift
+//  Repertoire d'entreprise
+//
+//  Created by Snoopy on 06/06/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class AccueilController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
